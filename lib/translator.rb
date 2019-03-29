322fa121119df0839337251ec28d6a4468b88237
Mallory Woods <mallorywoods@Mallorys-iMac.lan>
2019-03-29 16:52:37 -0400

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,12 @@
-# require modules here
+require 'pry'
+require "yaml"
 
-def load_library
-  # code goes here
+
+def load_library(path)
+  emoticons = YAML.load_file(path)
+  emoticons.each do |name, symbols|
+  
+  end
 end
 
 def get_japanese_emoticon
